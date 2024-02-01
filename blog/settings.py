367069339d43d297f0a1b430b00f8eb9f108e833
Kapin Patel <103712227+KapinPatel6261221355@users.noreply.github.com>
2024-02-01 15:13:45 +0530

    add loginRegister

--- blog/settings.py
+++ blog/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = 'static/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='article:index'
